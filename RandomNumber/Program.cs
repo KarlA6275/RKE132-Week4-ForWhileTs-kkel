a45b3910452f4024e596767f7531ccf7fe0ab39a
KarlA <Karl.adamson@tktk.ee>
2024-03-05 19:59:39 +0200

    add week4 code

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Random rnd  = new Random();
+
+int ayRandomNum;
+
+int randomsum = 0;
+
+for (int i = 0; i < 7; i++)
+{
+    ayRandomNum = rnd.Next(0, 11); // 0 - 10
+    randomsum = randomsum + ayRandomNum;
+    Console.WriteLine($"My random number is: {ayRandomNum}");
+}
+
+Console.WriteLine($"random sum total: {randomsum}");
